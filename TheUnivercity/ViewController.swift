ea4dd3b3bb324e319e0aca231bd2a092a702c0f5
Kutman Kakiev <Kutman.kakiev@gmail.com>
2023-01-25 06:15:38 +0900

    Initial Commit

---
+++ TheUnivercity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheUnivercity
+//
+//  Created by Kutman Kakiev on 25/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
